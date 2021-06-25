0f0150fd91b4b9388b21704e82e3d58e03faac2b
zeromomentum <zero@tilde.team>
2021-06-25 14:45:17 -0500

    a

--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,7 @@
-const purgecss = require('@fullhuman/postcss-purgecss')({
-  content: [
-    './src/**/*.html',
-    './src/**/*.svelte'
-  ],
-
-  whitelistPatterns: [/svelte-/],
-
-  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-});
-
-const production = !process.env.ROLLUP_WATCH
-
 module.exports = {
-  plugins: [
-    require('autoprefixer'),  
-    require('tailwindcss'),
-    ...(production ? [purgecss] : [])
-  ]
+  plugins: {
+	  require("autoprefixer"),
+	  require("tailwindcss")
+
+  },
 };
